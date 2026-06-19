7db116c59ff63b1e22c436dad5d0448ac4df1a35
Aviral Gupta <aviral.saya@gmail.com>
2026-06-18 22:16:52 -0500

    Update fetch_matches and sample_match json

--- fetch_matches.py
+++ fetch_matches.py
@@ -5,49 +5,70 @@ import json
 
 MATCH_ID = "4667751"
 
-url = (
+## Fetch Match details
+url_details = (
     f"https://www.fotmob.com/api/data/matchDetails"
     f"?matchId={MATCH_ID}"
 )
 
-response = requests.get(url)
+response_details = requests.get(url_details)
 
-print(response.status_code)
-# print(response.json())
+print(response_details.status_code)
 
-data = response.json()
+data_details = response_details.json()
 
 with open("sample_match.json", "w", encoding="utf-8") as f:
-    json.dump(data, f, indent=4)
+    json.dump(data_details, f, indent=4)
 
-print("JSON saved successfully!")
+print("Match Details JSON saved successfully!")
+
+# ## Fetch Match Insights
+# url_Insights = (
+#     f"https://www.fotmob.com/api/data/matchInsights"
+#     f"?matchId={MATCH_ID}"
+# )
+
+# response_insights = requests.get(url_Insights)
+
+# print(response_insights.status_code)
+# # print(response_details.json())
+
+# data_insights = response_insights.json()
+
+# with open("sample_match_insights.json", "w", encoding="utf-8") as f:
+#     json.dump(data_insights, f, indent=4)
+
+# print("Match Insights JSON saved successfully!")
 
 # print(data.keys())
 
 # print(data["general"].keys())
 
+## Data for Matches table
 match_data = {
-    "match_id": data["general"]["matchId"],
-    "competition": data["general"]["leagueName"],
-    "match_date": data["general"]["matchTimeUTCDate"],
+    "match_id": data_details["general"]["matchId"],
+    "competition": data_details["general"]["leagueName"],
+    "match_date": data_details["general"]["matchTimeUTCDate"],
 
-    "home_team": data["general"]["homeTeam"]["name"],
-    "away_team": data["general"]["awayTeam"]["name"],
+    "home_team": data_details["general"]["homeTeam"]["name"],
+    "away_team": data_details["general"]["awayTeam"]["name"],
 
-    "home_score": data["header"]["teams"][0]["score"],
-    "away_score": data["header"]["teams"][1]["score"],
+    "home_score": data_details["header"]["teams"][0]["score"],
+    "away_score": data_details["header"]["teams"][1]["score"],
 
     "winner": (
-        data["general"]["homeTeam"]["name"]
-        if data["header"]["teams"][0]["score"] >
-           data["header"]["teams"][1]["score"]
-        else data["general"]["awayTeam"]["name"]
+        data_details["general"]["homeTeam"]["name"]
+        if data_details["header"]["teams"][0]["score"] >
+           data_details["header"]["teams"][1]["score"]
+        else data_details["general"]["awayTeam"]["name"]
     )
 }
 
-##Match Stats###
+print ("Match_Data: ", match_data);
+
+## Data for Match Stats table
 match_stats = {
-    "match_id": match_id,
+    "match_id": match_data["match_id"],
 
     "possession_home": 60,
     "possession_away": 40,
@@ -62,13 +83,68 @@ match_stats = {
     "shots_on_target_away": 2
 }
 
-events = []
+print ("Match_Stats: ", match_stats);
+
+## Data for Match Events table
+event_list = data_details["content"]["matchFacts"]["events"]["events"]
+
+event_records = []
 
 for event in event_list:
 
-    events.append({
-        "minute": event["minute"],
-        "event_type": event["eventType"],
-        "player": event["playerName"],
-        "team": event["teamName"]
-    })
+    record = {
+        "event_id": event.get("eventId"),
+
+        "match_id": match_data["match_id"],
+
+        "minute": event.get("time"),
+
+        "minute_added": event.get("overloadTime"),
+
+        "event_type": event.get("type"),
+
+        "player_id": event.get("playerId"),
+
+        "player_name": event.get("fullName"),
+
+        "team_side": "HOME" if event.get("isHome") else "AWAY",
+
+        "home_score_before": event.get("homeScore"),
+
+        "away_score_before": event.get("awayScore"),
+
+        "home_score_after": (
+            event.get("newScore")[0]
+            if event.get("newScore")
+            else None
+        ),
+
+        "away_score_after": (
+            event.get("newScore")[1]
+            if event.get("newScore")
+            else None
+        ),
+
+        "assist_player_id": event.get("assistPlayerId"),
+
+        "assist_player_name": event.get("assistInput"),
+
+        "is_own_goal": (
+            event.get("ownGoal")
+            if event.get("ownGoal") is not None
+            else False
+        ),
+
+        "expected_goals": (
+            event.get("shotmapEvent", {})
+                 .get("expectedGoals")
+        ),
+
+        "event_description": (
+            event.get("assistStr")
+        )
+    }
+
+    event_records.append(record)
+    
+print ("Event_Records: ", event_records[0]);
